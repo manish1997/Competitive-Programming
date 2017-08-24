988b722a02006fc0c363c319188cbbc522eae4fb
Manish <manu1.uec2014@gmail.com>
2017-08-25 04:46:00 +0530

    Added Nasty Queries

--- Codechef/COOK82D.cpp
+++ Codechef/COOK82D.cpp
@@ -1,4 +1,4 @@
-//TODO: SQRT Decomposition will give TLE, couldn't get hashing approach
+//TODO: SQRT Decomposition will give TLE, couldn't get hash2ing approach
 #include <iostream>
 #include <cmath>
 #include <climits>
@@ -23,57 +23,54 @@ using namespace std;
 #define BLOCK 1000
 #define MAX 1000050
 #define MAXN 100005
-pair<ll,ll> arr[MAX];
 ll n,m;
-struct node {
-	int L, R, i;
-}q[MAX];
-unordered_map<int,bool> M;
-ll cnt[MAXN];
-bool cmp(node x, node y) {
-	if(x.L/BLOCK != y.L/BLOCK) {
-		return x.L/BLOCK < y.L/BLOCK;
-	}
-	return x.R < y.R;
-}
 bool ans;
 bool answer[MAX];
-
+int arr[100002];
+ll pre[1000002][8];
+ll preHash[8];
+ll pow2[8][100002];
 int main() {
 	sll(n); sll(m);
+	rep(j,2,7)
+	for(int i=0; i<100002; i++){
+		if(i==0) pow2[j][i]=1;
+		else{
+			pow2[j][i]=(pow2[j][i-1]*j)%mod;
+		}
+	}
 	for(int i=1; i<=m; i++){
 		ll x,y; sll(x); sll(y);
-		
-		cnt[x]++;
-		if(cnt[x]%2){
-			M[x]=true;
-			ans=false;
+		rep(j,2,7){
+			if(arr[x]==1){
+				preHash[j]=(preHash[j]-pow2[j][x])%mod;
+				while(preHash[j]<0) preHash[j]+=mod;
+				arr[x]=0;
+			}
+			else{
+				preHash[j]=(preHash[j]+pow2[j][x])%mod;
+				arr[x]=1;
+			}
+			if(arr[y]==1){
+				preHash[j]=(preHash[j]-pow2[j][y])%mod;
+				while(preHash[j]<0) preHash[j]+=mod;
+				arr[y]=0;
+			}
+			else{
+				preHash[j]=(preHash[j]+pow2[j][y])%mod;
+				arr[y]=1;
+			}
+			pre[i][j]=preHash[j];
 		}
-		else{
-			M.erase(M.find(x));
-			if(M.size()==0) ans=true;
-		}
-		cnt[y]++;
-		if(cnt[y]%2){
-			M[y]=true;
-			ans=false;
-		}
-		else{
-			M.erase(M.find(y));
-			if(M.size()==0) ans=true;
-		}
-		if(ans) answer[i]=1;
-		else answer[i]=0;
-		//cout << x << " " << cnt[x] << " " << y << " " << cnt[y]<< endl;
-		cout << answer[i] << " ";
 	}
-	answer[0]=0;
-	cout << endl;
 	int qq;
 	si(qq);
 	for(int i=1; i<=qq; i++){
-		si(q[i].L); si(q[i].R);
-		if(answer[q[i].L-1]==answer[q[i].R]) printf("Yes\n");
+		int a,b;
+		si(a); si(b);
+		bool pp=true;
+		rep(j,2,7) if(pre[a-1][j]!=pre[b][j]) pp=false;
+		if(pp) printf("Yes\n");
 		else printf("No\n");
 	}
 }
