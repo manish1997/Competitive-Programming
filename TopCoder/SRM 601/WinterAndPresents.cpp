946258eab2b9b7936f2c1de2a50dff6247f5e152
Manish <manu1.uec2014@gmail.com>
2017-09-14 03:09:14 +0530

    SRM 601,603 div 1, 250ptr

---
+++ TopCoder/SRM 601/WinterAndPresents.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i, start, end) for(ll i=start; i<end; i++)
+#define repDown(i, start, end) for(ll i=start; i>=end; i--)
+#define MAX 500005
+
+using namespace std;
+
+class WinterAndPresents {
+    public:
+    long long getNumber(vector<int> apple, vector<int> orange) {
+        int MIN=INT_MAX;
+        rep(i,0,apple.size()) MIN=min(MIN,apple[i]+orange[i]);
+        if(MIN==0) return 0;
+        ll ans=0;
+        repDown(i,MIN,1){
+            ll A=0,B=0;
+            rep(j,0,apple.size()){
+                ll a=apple[j];
+                ll b=orange[j];
+                if(a>b) swap(a,b);
+                ll bMin=max(0LL,i-a);
+                ll bMax=min(i,b);
+                A+=bMin;
+                B+=bMax;
+            }
+            ans+=(B-A+1);
+        }
+        return ans;
+    }
+};
+
+// CUT badsaegin
+ifstream data("WinterAndPresents.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> apple, vector<int> orange, long long __expected) {
+    time_t startClock = clock();
+    WinterAndPresents *instance = new WinterAndPresents();
+    long long __result = instance->getNumber(apple, orange);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED !" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED !" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> apple;
+        from_stream(apple);
+        vector<int> orange;
+        from_stream(orange);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(apple, orange, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed qith : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1505264531;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time  sadad : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  asda: " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "WinterAndPresents (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
