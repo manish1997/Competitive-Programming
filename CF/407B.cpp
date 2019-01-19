9ad4bc93e0b7d7ee617f45120d8557f8b2fc26e6
Manish Kumar <mm000143@gmail.com>
2019-01-20 03:41:19 +0530

    adding 407B

--- CF/407B.cpp
+++ CF/407B.cpp
@@ -1,49 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX 105
-int n,m;
+#define mod 1000000007
 #define ll long long
-ll dp[MAX][MAX];
-ll arr[MAX][MAX];
 
-bool valid(int i, int j){
-    // cout << i << " " << j <<" " << n << " " << m << " " << (i>=0 && i<n && j>=0 && j<m)<< endl;
-    return (i>=0 && j>=0 && i<n && j<m);
-}
-ll helper(int x, int y){
-    if(valid(x,y)==false) return INT_MIN;
-    // cout << x << " " << y <<endl;
-    if(dp[x][y]!=-1) return dp[x][y];
-    ll ans=0;
-    for(int i=-1; i<=1; i++){
-        for(int j=-1; j<=1; j++){
-            if(i==0 && j==0) continue;
-            int nx=x+i;
-            int ny=y+j;
-            if(valid(nx,ny)==false || arr[nx][ny]<=arr[x][y]) continue;
-            ans=max(ans, helper(nx,ny));
-        }
-    }
-    return dp[x][y]=(ans+1);
-}
-int main(){
-    cin>>n>>m;
 
+void solve(){
+    int n;cin>>n;
+    ll dp[n+1];
+    ll arr[n];
     for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin>>arr[i][j];
-        }
+        cin>>arr[i];
     }
-    ll ans=0;
-    memset(dp,-1,sizeof(dp));
-    cout << helper(0,0) << endl;
-    // for(int i=0; i<n; i++){
-    //     for(int j=0; j<m; j++){
-    //         // cout << helper(i,j) << endl;
-    //         ans=max(ans, helper(i,j));
-    //     }
-    // }
-    cout << ans << endl;
+    dp[0]=0;
+    for(int i=0; i<n; i++){
+        dp[i+1]=(((2*dp[i])%mod)-dp[arr[i]-1]+2+mod)%mod;
+    }
+    cout << dp[n] << endl;
+}
+
+int main(){
+    int t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
 
 
     return 0;
